78e7b12de24d3fc75c82413a0b76540b1bc80dc5
RussSal777 <rscarter71@gmail.com>
2025-06-24 16:56:23 -0500

    Almost almost there

--- app.js
+++ app.js
@@ -1,81 +1,59 @@
-// As a user, I want to be able to select numbers so that I can perform operations with them.
-// As a user, I want to be able to add two numbers together.
-// As a user, I want to be able to subtract one number from another.
-// As a user, I want to be able to multiply two numbers together.
-// As a user, I want to be able to divide one number by another.
-// As a user, I want to be able to see the output of the mathematical operation.
-// As a user, I want to be able to clear all operations and start from 0.
-
-
 /*-------------------------------- Constants --------------------------------*/
 
-const numberButtons = document.querySelectorAll('.button.number');
-const operatorButtons = document.querySelectorAll('.button.operator');
-const equalsButton = document.querySelector('.button.equals');
-const display = document.querySelector('.display');
-const calculator = document.querySelector('#calculator');
+const numberButtons = document.querySelectorAll(".button.number");
+const operatorButtons = document.querySelectorAll(".button.operator");
+const equalsButton = document.querySelector(".button.equals");
+const display = document.querySelector(".display");
+const calculator = document.querySelector("#calculator");
 
 /*-------------------------------- Variables --------------------------------*/
 
-let firstNumber = '';
-let currentNumber = '';
+let firstNumber = "";
+let currentNumber = "";
 let operator = null;
-let displayText = '';
 
 /*------------------------ Cached Element References ------------------------*/
-
-
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 for (let i = 0; i < numberButtons.length; i++) {
-  numberButtons[i].addEventListener('click', function(event) {
-    currentNumber += event.target.innerText;
-      display.innerText = currentNumber;
-  })
+  numberButtons[i].addEventListener("click", function (event) {
+    secondNumber += event.target.innerText;
+    display.innerText = secondNumber;
+  });
 }
-
-/* good above */
 
 for (let i = 0; i < operatorButtons.length; i++) {
-  operatorButtons[i].addEventListener('click', function(event) {
-    console.log(event.target);
-    if (event.target.textContent === 'C') {
-      display.textContent = '0';
-      firstNumber = '';
-      currentNumber = '';
-      console.log(firstNumber, currentNumber, operator);
+  operatorButtons[i].addEventListener("click", function (event) {
+    if (event.target.textContent === "C") {
+      display.textContent = "0";
+      firstNumber = "";
+      secondNumber = "";
     } else {
       operator = event.target.innerText;
-      firstNumber = currentNumber;
-      currentNumber = '';
+      firstNumber = secondNumber;
+      secondNumber = "";
       display.innerText = operator;
     }
-  })
+  });
 }
 
-
-
-// good below
-
-
-equalsButton.addEventListener('click', function(event) {
+equalsButton.addEventListener("click", function (event) {
   let result;
   const num1 = Number(firstNumber);
   const num2 = Number(currentNumber);
 
-  if (operator === '+') {
+  if (operator === "+") {
     result = num1 + num2;
-  } else if (operator === '-') {
+  } else if (operator === "-") {
     result = num1 - num2;
-  } else if (operator === '*') {
+  } else if (operator === "*") {
     result = num1 * num2;
-  } else if (operator === '/') {
+  } else if (operator === "/") {
     result = num1 / num2;
   }
   display.innerText = result;
 });
 
 /*-------------------------------- Functions --------------------------------*/
-
 
