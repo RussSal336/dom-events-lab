0f101fd1a32ff5b6b4398cef741f01becbcc2154
RussSal777 <rscarter71@gmail.com>
2025-06-24 17:39:45 -0500

    Lab complete

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const numberButtons = document.querySelectorAll(".button.number");
-const operatorButtons = document.querySelectorAll(".button.operator");
-const equalsButton = document.querySelector(".button.equals");
-const display = document.querySelector(".display");
-const calculator = document.querySelector("#calculator");
+const numberButtons = document.querySelectorAll('.button.number');
+const operatorButtons = document.querySelectorAll('.button.operator');
+const equalsButton = document.querySelector('.button.equals');
+const display = document.querySelector('.display');
+const calculator = document.querySelector('#calculator');
 
 /*-------------------------------- Variables --------------------------------*/
 
-let firstNumber = "";
-let currentNumber = "";
+let firstNumber = '';
+let currentNumber = '';
 let operator = null;
 
 /*------------------------ Cached Element References ------------------------*/
@@ -17,43 +17,46 @@ let operator = null;
 /*----------------------------- Event Listeners -----------------------------*/
 
 for (let i = 0; i < numberButtons.length; i++) {
-  numberButtons[i].addEventListener("click", function (event) {
-    secondNumber += event.target.innerText;
-    display.innerText = secondNumber;
+  numberButtons[i].addEventListener('click', function (event) {
+    currentNumber += event.target.innerText;
+    display.innerText = currentNumber;
   });
 }
 
 for (let i = 0; i < operatorButtons.length; i++) {
-  operatorButtons[i].addEventListener("click", function (event) {
-    if (event.target.textContent === "C") {
-      display.textContent = "0";
-      firstNumber = "";
-      secondNumber = "";
+  operatorButtons[i].addEventListener('click', function (event) {
+    if (event.target.textContent === 'C') {
+      display.textContent = '0';
+      firstNumber = '';
+      currentNumber = '';
     } else {
       operator = event.target.innerText;
-      firstNumber = secondNumber;
-      secondNumber = "";
+      firstNumber = currentNumber;
+      currentNumber = '';
       display.innerText = operator;
     }
   });
 }
 
-equalsButton.addEventListener("click", function (event) {
+equalsButton.addEventListener('click', function (event) {
   let result;
   const num1 = Number(firstNumber);
   const num2 = Number(currentNumber);
 
-  if (operator === "+") {
+  if (operator === '+') {
     result = num1 + num2;
-  } else if (operator === "-") {
+  } else if (operator === '-') {
     result = num1 - num2;
-  } else if (operator === "*") {
+  } else if (operator === '*') {
     result = num1 * num2;
-  } else if (operator === "/") {
+  } else if (operator === '/') {
     result = num1 / num2;
   }
   display.innerText = result;
+  firstNumber = result;
+  currentNumber = result;
+  operator = null;
 });
 
 /*-------------------------------- Functions --------------------------------*/
-
+ 
